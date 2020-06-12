b9d8a1fd22a072ec765982741792eaa26595c5d7
Aleksei Robert Cruz <alekseicruz@gmail.om>
2020-06-12 00:41:37 -0700

    Starting from scratch, implemented ES6 enabled server.

--- server/routes/IORoutes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (io) => {
-    io.on('connection', (socket) => {
-        socket.on('chat message', (msg) => {
-            console.log(msg);
-            io.emit('chat message', msg);
-        });
-    });
-};
