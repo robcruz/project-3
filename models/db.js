f59d332f94cb79c13354dad5e25703af87cf561d
Robert Gaerlan <gaerlanrobert@gmail.com>
2020-06-17 20:08:11 -0700

    refactored server.js for heroku deployment

--- models/db.js
+++ models/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // Connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/swap_me', { useNewUrlParser: true });
+// mongodb://<dbuser>:<dbpassword>@ds011228.mlab.com:11228/heroku_mf311fz0
 // Validation
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
